49162cc01f21fb673739956aea28ea31357d79ba
wujinlin <wujinlin@baidu.com>
2017-02-14 13:39:03 +0800

    先加个数据库测试资源路径及controller
    Change-Id: Iee2b00372804fd8b9b14a9c99c708844d0c1c5a3

---
+++ mkt-iac-api/src/main/java/com/baidu/bce/mkt/iac/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+// Copyright 2017 Baidu Inc. All rights reserved.
+
+package com.baidu.bce.mkt.iac.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * authentication controller
+ *
+ * @author Wu Jinlin(wujinlin@baidu.com)
+ */
+@RestController
+@RequestMapping("/v1/authentication")
+@Slf4j
+public class AuthenticationController {
+}
