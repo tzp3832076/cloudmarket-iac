b66522a9ba153ffa37cb027e16904535f457acfb
wujinlin <wujinlin@baidu.com>
2017-03-03 10:50:46 +0800

    再更新一下发布脚本
    Change-Id: Icdd18b1e8c6022cef6e74cadd8e2883d4fa08805

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,6 +3,6 @@ export JAVA_HOME=$BUILD_KIT_PATH/java/jdk-1.8-8u20
 export MAVEN_3_0_4=/usr/maven/apache-maven-3.0.4/bin
 export PATH=${JAVA_HOME}/bin:/usr/maven/apache-maven-3.0.4/bin:$PATH 
 
-find ./  -name "pom.xml" -exec sed -i "s/<mkt-iac-version>version/<mkt-iac-version>$1/g" {} \;
+find ./  -name "pom.xml" -exec sed -i "s/<version>\${mkt-iac-version}/<version>$1/g" {} \;
 
 mvn -Dversion=$1 -Dmaven.test.skip=true clean package deploy:deploy -U
