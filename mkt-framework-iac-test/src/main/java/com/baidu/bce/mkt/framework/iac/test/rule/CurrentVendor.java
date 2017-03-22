95d65742e288dc7ce5b3a9195e5c69f92b0c6c4a
wujinlin <wujinlin@baidu.com>
2017-03-22 14:28:48 +0800

    增加iac的测试类
    Change-Id: Ie74c06f1966cb14c80bb898ae26293c2ca02e428

---
+++ mkt-framework-iac-test/src/main/java/com/baidu/bce/mkt/framework/iac/test/rule/CurrentVendor.java
@@ -0,0 +1,20 @@
+// Copyright 2017 Baidu Inc. All rights reserved.
+
+package com.baidu.bce.mkt.framework.iac.test.rule;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * current vendor
+ *
+ * @author Wu Jinlin(wujinlin@baidu.com)
+ */
+@Target(value = {ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentVendor {
+    String vendorId() default "";
+    boolean hasId() default true;
+}
