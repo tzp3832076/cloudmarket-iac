0c13a116b9f13a442c508c15be979131828b23e7
wujinlin <wujinlin@baidu.com>
2017-07-05 15:54:33 +0800

    清除的逻辑调整一下
    Change-Id: I72936d369b012f7c6aa0f23f684ea968cb705537

--- mkt-framework-iac/src/main/java/com/baidu/bce/mkt/framework/iac/web/CheckAuthWebInterceptor.java
+++ mkt-framework-iac/src/main/java/com/baidu/bce/mkt/framework/iac/web/CheckAuthWebInterceptor.java
@@ -56,8 +56,8 @@ public class CheckAuthWebInterceptor extends HandlerInterceptorAdapter {
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response,
-                           Object handler, ModelAndView modelAndView) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                Exception ex) throws Exception {
         TokenHolder.removeAuthorizedToken();
     }
 }
