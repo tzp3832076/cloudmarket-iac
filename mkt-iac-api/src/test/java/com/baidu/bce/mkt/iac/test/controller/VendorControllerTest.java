3f7ff8f4a298546e646ddf733c2321b7f56b1bbf
wangbin33 <wangbin33@baidu.com>
2018-08-13 15:32:19 +0800

    APT-3215
    Change-Id: Ia776a81cb4ffdbc6c595b499dcd921720cdf9847

--- mkt-iac-api/src/test/java/com/baidu/bce/mkt/iac/test/controller/VendorControllerTest.java
+++ mkt-iac-api/src/test/java/com/baidu/bce/mkt/iac/test/controller/VendorControllerTest.java
@@ -145,10 +145,10 @@ public class VendorControllerTest extends ApiMockMvcTest {
             Assert.fail("no exception");
         } catch (BceInternalResponseException e) {
             Assert.assertTrue(e.getMessage().contains(
-                    "baiduQiaos=客服信息需要为夜莺平台：https://ikefu.baidu.com"));
+                    "baiduQiaos=客服信息需要为商桥平台：http://p.qiao.baidu.com"));
         }
         onlineSupports.clear();
-        onlineSupports.add(new OnlineSupport("test", "https://ikefu.baidu.com/ddd"));
+        onlineSupports.add(new OnlineSupport("test", "http://p.qiao.baidu.com/ddd"));
         mktIacClient.submitVendorShopDraft(request);
 
     }
